8b59931d243ec83ba032ced36bed3b1fed15ad8d
wajdibabdallah <wajdibabdallah@gmail.com>
2020-09-04 23:20:32 +0200

    init interceptor

--- src/app/shared/service/token-interceptor.service.ts
+++ src/app/shared/service/token-interceptor.service.ts
@@ -19,32 +19,6 @@ export class TokenInterceptorService implements HttpInterceptor {
     request: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    const token: string = localStorage.getItem('token');
-
-    if (token) {
-      request = request.clone({
-        headers: request.headers.set('Authorization', 'Bearer ' + token),
-      });
-    }
-
-    if (!request.headers.has('Content-Type')) {
-      request = request.clone({
-        headers: request.headers.set('Content-Type', 'application/json'),
-      });
-    }
-
-    request = request.clone({
-      headers: request.headers.set('Accept', 'application/json'),
-    });
-
-    return next.handle(request).pipe(
-      map((event: HttpEvent<any>) => {
-        console.log('event =>', event);
-        if (event instanceof HttpResponse) {
-          console.log('event--->>>', event);
-        }
-        return event;
-      })
-    );
+    return next.handle(request);
   }
 }
